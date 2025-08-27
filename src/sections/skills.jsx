ca3baa3d491c773d27ef97bf4a5a179f56404856
Thabith2001 <129141730+Thabith2001@users.noreply.github.com>
2025-08-27 20:17:45 +0530

    Update skills.jsx

--- src/sections/skills.jsx
+++ src/sections/skills.jsx
@@ -4,31 +4,31 @@ import Image from "next/image";
 
 const Skill = () => {
     const skills = {
-        java: { alt: "Java Logo", src: "/tech_icons/Java_i.png" },
-        javascript: { alt: "JavaScript Logo", src: "/tech_icons/JavaScript_i.png" },
-        html: { alt: "HTML Logo", src: "/tech_icons/Html5_i.png" },
-        css: { alt: "CSS Logo", src: "/tech_icons/Css3_i.png" },
-        react: { alt: "React Logo", src: "/tech_icons/React_i.png" },
-        nextjs: { alt: "Next.js Logo", src: "/tech_icons/Nextjs_i.png" },
-        tailwind: { alt: "Tailwind CSS Logo", src: "/tech_icons/Tailwind_i.png" },
-        bootstrap: { alt: "Bootstrap Logo", src: "/tech_icons/Bootstrap_i.png" },
-        mongodb: { alt: "MongoDB Logo", src: "/tech_icons/MongoDB_i.png" },
-        mysql: { alt: "MySQL Logo", src: "/tech_icons/MYSQL_i.png" },
-        spring_boot: { alt: "Spring Boot Logo", src: "/tech_icons/Spring_i.png" },
-        hibernate: { alt: "Hibernate Logo", src: "/tech_icons/Hibernate_i.png" },
-        php: { alt: "PHP Logo", src: "/tech_icons/Php_i.png" },
+        java: { alt: "Java Logo", src: "/tech_icons/java_i.png" },
+        javascript: { alt: "JavaScript Logo", src: "/tech_icons/javaScript_i.png" },
+        html: { alt: "HTML Logo", src: "/tech_icons/html5_i.png" },
+        css: { alt: "CSS Logo", src: "/tech_icons/css3_i.png" },
+        react: { alt: "React Logo", src: "/tech_icons/react_i.png" },
+        nextjs: { alt: "Next.js Logo", src: "/tech_icons/nextjs_i.png" },
+        tailwind: { alt: "Tailwind CSS Logo", src: "/tech_icons/tailwind_i.png" },
+        bootstrap: { alt: "Bootstrap Logo", src: "/tech_icons/bootstrap_i.png" },
+        mongodb: { alt: "MongoDB Logo", src: "/tech_icons/mongodb_i.png" },
+        mysql: { alt: "MySQL Logo", src: "/tech_icons/mysql_i.png" },
+        spring_boot: { alt: "Spring Boot Logo", src: "/tech_icons/spring_i.png" },
+        hibernate: { alt: "Hibernate Logo", src: "/tech_icons/hibernate_i.png" },
+        php: { alt: "PHP Logo", src: "/tech_icons/php_i.png" },
     };
 
     const idea = {
-        intellij: { alt: "IntelliJ Logo", src: "/tech_icons/Intellijidea_i.png" },
+        intellij: { alt: "IntelliJ Logo", src: "/tech_icons/intellijidea_i.png" },
         vscode: { alt: "Visual Studio Code Logo", src: "/tech_icons/vscode_i.png" },
         git: { alt: "Git Logo", src: "/tech_icons/git_i.png" },
         netbeans: { alt: "NetBeans Logo", src: "/tech_icons/netBeans_i.png" },
-        eclipse: { alt: "Eclipse Logo", src: "/tech_icons/Eclipse_i.png" },
-        android_studio: { alt: "Android Studio Logo", src: "/tech_icons/AndroidStudio_i.png" },
-        web_storm: { alt: "WebStorm Logo", src: "/tech_icons/Webstorm_i.png" },
-        phpstorm: { alt: "PhpStorm Logo", src: "/tech_icons/Phpstorm_i.png" },
-        dbeaver:{alt:"Dbeaver Logo",src:"/tech_icons/Dbeaver_i.png"},
+        eclipse: { alt: "Eclipse Logo", src: "/tech_icons/eclipse_i.png" },
+        android_studio: { alt: "Android Studio Logo", src: "/tech_icons/androidStudio_i.png" },
+        web_storm: { alt: "WebStorm Logo", src: "/tech_icons/webstorm_i.png" },
+        phpstorm: { alt: "PhpStorm Logo", src: "/tech_icons/phpstorm_i.png" },
+        dbeaver:{alt:"Dbeaver Logo",src:"/tech_icons/dbeaver_i.png"},
     };
 
     return (
