ba4756791ef576b411ed8efe47169c63d4b09e8e
thabith <thabith20010909@gmail.com>
2025-08-29 16:06:41 +0530

    Add floating button component

---
+++ src/data/achievements_data.js
@@ -0,0 +1,37 @@
+export const education = [
+    {
+        year: "2023 - 2024",
+        title: "Bachelor Degree",
+        place: "Birmingham City University",
+        desc: "Completed my Bachelor of Science in Software Engineering at Birmingham City University with a second Upper Division Honours degree.",
+        logo: "/education/bcu_i.png",
+    },
+    {
+        year: "2022 - 2023",
+        title: "Professional Higher National Diploma",
+        place: "UK Awards",
+        desc: "Completed Level 5 Professional Higher National Diploma in Software Engineering from UK Awards.",
+        logo: "/education/uka_i.png",
+    },
+    {
+        year: "2021 - 2022",
+        title: "Professional Diploma",
+        place: "UK Awards",
+        desc: "Completed Level 4 Professional Diploma in Software Engineering from UK Awards.",
+        logo: "/education/uka_i.png",
+    },
+    {
+        year: "2018 - 2019",
+        title: "Advanced Level",
+        place: "Lumbini College",
+        desc: "Discontinued my studies at Lumbini College due to some issues.",
+        logo: "/education/school_i.png",
+    },
+    {
+        year: "2007 - 2017",
+        title: "Ordinary Level",
+        place: "Lumbini College",
+        desc: "Completed my studies at Lumbini College.",
+        logo: "/education/school_i.png",
+    },
+];
---
+++ src/data/projects_data.js
@@ -0,0 +1,43 @@
+export const projects_data = [
+    {
+        id: 1,
+        title: 'E-Commerce App',
+        description:
+            'An online shopping platform built with Spring Boot providing a secure and scalable backend. Key features include user authentication (JWT-based login/signup), role-based access control, product catalog management, and shopping cart functionality. The system is designed with a modular architecture, using RESTful APIs, Spring Security, and JPA/Hibernate for robust database integration.',
+        tags: ['Spring Boot', 'MySQL','jwt','spring-security','jpa','hibernate','restful-api'],
+        image: '/images/sp1.png',
+        codeLink: 'https://github.com/Thabith2001/e-commerce',
+        deployLink: '',
+    },
+    {
+        id: 2,
+        title: 'Music Streaming Application',
+        description:
+            'A full-featured music streaming platform built using Java Gradle, allowing users to browse, stream, and manage music playlists in real time. The application supports secure user authentication via Google and Facebook OAuth APIs, providing a seamless login experience.',
+        tags: ['xml', 'Java Gradle','roomDB','firebase','google-oauth','facebook-oauth'],
+        image: '/images/mob.jpeg',
+        codeLink: 'https://github.com/Thabith2001/Music-Streaming-Application',
+        deployLink: '',
+
+    },
+    {
+        id: 3,
+        title: 'Green Basket - Vegetable Shop Web App',
+        description:
+            'Green Basket is a responsive vegetable shop web application developed using React, Tailwind CSS, and JSON server. It allows users to browse fresh vegetables, view prices, and add items to their cart. The project includes dynamic routing, reusable components, and a clean UI design.  UNDER DEVELOPMENT',
+        tags: ['React', 'Tailwind CSS', 'JSON Server'],
+        image: '/images/greenbasket.png',
+        codeLink: 'https://github.com/Thabith2001/Green-Basket',
+        deployLink: 'https://green-basket-woad.vercel.app/',
+    },
+    {
+        id: 4,
+        title: 'Portfolio Website',
+        description:
+            'Personal portfolio website built with Next.js and Tailwind CSS. It showcases my skills, projects, and contact information. The website is responsive and optimized for mobile devices.',
+        tags: ['Next.js', 'Tailwind CSS', 'React'],
+        image: '/images/portfolio_i.png',
+        codeLink: 'https://github.com/Thabith2001/thabith-s-_portfolio',
+        deployLink: 'https://thabithportfolio-beryl.vercel.app/',
+    }
+];
